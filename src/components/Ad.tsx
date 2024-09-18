58927125b2ae28c8f16e3c6547bcb5b9c004b86c
nftcryptomix-admin <steevcan@gmail.com>
2024-09-18 14:45:33 +1200

    Adding side bar

---
+++ src/components/Ad.tsx
@@ -0,0 +1,9 @@
+const Ad = ({size}: {size:"sm" | "md" | "lg"}) => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm">
+      Ad
+    </div>
+  )
+};
+
+export default Ad;
--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,6 +1,14 @@
-const RightMenu = () => {
+import Ad from "./Ad";
+import Birthdays from "./Birthdays";
+import FriendRequests from "./FriendRequests";
+
+const RightMenu = ({ userId }: { userId?: string }) => {
   return (
-    <div className="">RightMenu</div>
+    <div className="flex flex-col gap-6">
+      <FriendRequests/>
+      <Birthdays/>
+      <Ad size="md"/>
+    </div>
   )
 };
 
